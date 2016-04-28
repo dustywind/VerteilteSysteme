7d28bb59765fb31986ee7c4e2830119e14b732fd
Dustin Wind <dusty@posteo.de>
2016-04-28 20:44:11 +0200

    added tests for different datatypes

--- VSTask01/src/vsue/rmi/VSClient.java
+++ VSTask01/src/vsue/rmi/VSClient.java
@@ -15,13 +15,47 @@ public class VSClient {
             VSConnection conn = new VSConnection(s);
             VSObjectConnection objConn = new VSObjectConnection(conn);
             
-            objConn.sendObject(TestObjects.primitiveInteger);
-            int receivedPrimitiveInteger = (Integer) objConn.receiveObject();
-            
-            assert receivedPrimitiveInteger == TestObjects.primitiveInteger;
+            System.out.println(testConnection(objConn));
         }
         catch(Exception e){
             e.printStackTrace();
         }
     }
+    
+    private static boolean testConnection(VSObjectConnection objConn) throws Exception{
+        
+        boolean success = true;
+        
+        //success &= testPrimitiveInteger(objConn);
+        //success &= testWrappedInteger(objConn);
+        //success &= testShortString(objConn);
+        success &= testVeryLongString(objConn);
+        
+        return success;
+    }
+    
+    private static boolean testPrimitiveInteger(VSObjectConnection objConn) throws Exception{
+        objConn.sendObject(TestObjects.primitiveInteger);
+        int receivedPrimitiveInteger = (Integer) objConn.receiveObject();
+        return  receivedPrimitiveInteger == TestObjects.primitiveInteger;
+    }
+    
+    private static boolean testWrappedInteger(VSObjectConnection objConn) throws Exception{
+        objConn.sendObject(TestObjects.wrappedInteger);
+        int receivedWrappedInteger = (Integer) objConn.receiveObject();
+        return  receivedWrappedInteger == TestObjects.wrappedInteger;
+    }
+    
+    private static boolean testShortString(VSObjectConnection objConn) throws Exception{
+        objConn.sendObject(TestObjects.shortString);
+        String receivedString = (String) objConn.receiveObject();
+        return receivedString.compareTo(TestObjects.shortString) == 0;
+    }
+    
+    private static boolean testVeryLongString(VSObjectConnection objConn) throws Exception{
+        objConn.sendObject(TestObjects.veryLongString);
+        String receivedString = (String) objConn.receiveObject();
+        return receivedString.compareTo(TestObjects.veryLongString) == 0;
+    }
+    
 }
