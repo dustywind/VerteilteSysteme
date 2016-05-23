43ed5d83b6a8600371d1a704886308289c8a930a
Dustin Wind <dusty@posteo.de>
2016-05-23 23:03:32 +0200

    added class I forgot to add earlier

---
+++ VSTask02/src/vsue/rmi/ReflectionHelper.java
@@ -0,0 +1,38 @@
+package vsue.rmi;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class ReflectionHelper {
+    
+    public static Class<?>[] getAllInterfaces(Class<?> cl){
+        Set<Class<?>> interfaces = new TreeSet<Class<?>>();;
+        
+        for(; cl != null; cl = cl.getSuperclass()){
+            for(Class<?> i : cl.getInterfaces()){
+                interfaces.add(i);
+            }
+        }
+        
+        return interfaces.toArray(new Class<?>[0]);
+        
+    }
+    
+    public static Method getMatchingMethod(Class<?> cl, String methodName, Object[] args){
+        
+        Method match = null;
+        for(Method m : cl.getMethods()){
+            if(m.getName().compareTo(methodName) == 0){
+                Class<?>[] argTypes = m.getParameterTypes();
+                // TODO this is not reliable
+                if(argTypes.length == args.length){
+                    match = m;
+                }
+            }
+        }
+        
+        return match;
+    }
+}
